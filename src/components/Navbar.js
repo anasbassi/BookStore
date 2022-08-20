c2159442812573a17b5f02b1e9db57a1f5651e9a
Anas Abubakar <anasabubakar2015@gmail.com>
2022-08-20 14:41:45 +0100

    Add font to the text

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom';
 const Navbar = () => (
   <header>
     <div className="nav-container">
-      <h1>Book Store</h1>
+      <h1>BookStore</h1>
       <nav>
         <Link to="/">Books</Link>
         <Link to="/categories">Categories</Link>
